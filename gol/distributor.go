51695ea1f6a7ba4ec483303e17f8cf53c4005793
ek22528 <ek22528@bristol.ac.uk>
2023-10-30 16:24:29 +0000

    parallelises the solution, all tests pass

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,8 +1,8 @@
 package gol
 
 import (
-	"fmt"
   "uk.ac.bris.cs/gameoflife/util"
+  "fmt"
 )
 
 type distributorChannels struct {
@@ -12,6 +12,32 @@ type distributorChannels struct {
 	ioFilename chan<- string
 	ioOutput   chan<- uint8
 	ioInput    <-chan uint8
+}
+
+func distribute(world [][]byte, p Params) [][]byte {
+  channels := make([]chan [][]byte, p.Threads)
+  for i := range channels {
+    channels[i] = make(chan [][]byte)
+  }
+  heightDiff := p.ImageHeight / p.Threads  
+  // sets up workers for all except last slice
+  for i := 0; i < p.Threads - 1; i++ {
+    go worker(world, p, i * heightDiff, (i + 1) * heightDiff, channels[i])
+  }
+  // sets up worker for last slice, necessary to correct
+  // for inconsistencies with rounding
+  go worker(world, p, (p.Threads - 1) * heightDiff, p.ImageHeight, channels[p.Threads - 1])
+
+  var newWorld [][]byte
+  for i := 0; i < p.Threads; i++ {
+    thisSlice := <- channels[i]
+    newWorld = append(newWorld, thisSlice...)
+    //cells := getAliveCells(newWorld, p)
+    //if len(cells) != 0 {
+      //fmt.Println(cells)
+    //}
+  }
+  return newWorld 
 }
 
 func getAliveCells(world [][]byte, p Params) []util.Cell {
@@ -34,9 +60,9 @@ func distributor(p Params, c distributorChannels) {
 	c.ioFilename <- fmt.Sprintf("%d%s%d", p.ImageWidth, "x", p.ImageHeight)
 
 	// make 2d slice to hold world
-	world := make([][]byte, p.ImageWidth)
+	world := make([][]byte, p.ImageHeight)
 	for i := range world {
-		world[i] = make([]byte, p.ImageHeight)
+		world[i] = make([]byte, p.ImageWidth)
 		for j := range world[i] {
 			world[i][j] = <-c.ioInput
 		}
@@ -46,7 +72,7 @@ func distributor(p Params, c distributorChannels) {
 
 	// TODO: Execute all turns of the Game of Life.
 	for turn = 0; turn < p.Turns; turn++ {
-		world = executeTurn(world, p)
+		world = distribute(world, p)
 		c.events <- TurnComplete{CompletedTurns: turn}
 	}
 
--- gol/worker.go
+++ gol/worker.go
@@ -1,17 +1,18 @@
 package gol
 
-import (
-)
+func worker(world [][]byte, p Params, startY, endY int, out chan [][]byte) {
 
-func worker(world [][]byte, p Params, startY, endY int) [][]byte {
-	newworld := make([][]byte, p.ImageWidth)
+  // make a new slice that is the size of the part of the
+  // world to be computed by this worker
+	newworld := make([][]byte, endY - startY)
 	for i := range newworld {
-		newworld[i] = make([]byte, endY - startY)
+		newworld[i] = make([]byte, p.ImageWidth)
 	}
 
-	// check each cell in world
+	// check each cell in the appropriate portion of the world
   for i := range world[startY:endY] {
-		for j := range world[i] {
+    worldIndex := startY + i
+		for j := range world[worldIndex] {
 
 			sum := 0
 
@@ -19,23 +20,23 @@ func worker(world [][]byte, p Params, startY, endY int) [][]byte {
 			for m := -1; m <= 1; m++ {
 				for n := -1; n <= 1; n++ {
 					if m != 0 || n != 0 {
-						dx := (i + m + p.ImageWidth) % p.ImageWidth
-						dy := (j + n + p.ImageHeight) % p.ImageHeight
+						dy := (worldIndex + m + p.ImageHeight) % p.ImageHeight
+						dx := (j + n + p.ImageWidth) % p.ImageWidth
 
-						sum += (int(world[dx][dy]) / 255)
+						sum += (int(world[dy][dx]) / 255)
 
 					}
 				}
 			}
 
-			//apply rules
-			if sum == 3 || (int(world[i][j])/255)+sum == 3 {
-				newworld[i % (startY - endY)][j] = 255
+			// apply rules
+			if sum == 3 || (int(world[worldIndex][j])/255)+sum == 3 {
+				newworld[i][j] = 255
 			}
 		}
 	}
-
-	return newworld
+  // send computed data to the channel provided by the arguments
+  out <- newworld
 }
 
 
