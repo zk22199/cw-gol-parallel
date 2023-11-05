7188fa0b8b88ec867e3d64d50fd89635548108ec
ek22528 <ek22528@bristol.ac.uk>
2023-11-05 19:49:38 +0000

    changes the worker class to simplify cell flipped events calling

--- gol/worker.go
+++ gol/worker.go
@@ -22,7 +22,7 @@ func worker(world [][]byte, p Params, c distributorChannels, turn int, startY, e
 
 		for j := range world[worldIndex] {
 
-			sum := 0
+			var sum uint8 = 0
 
 			// counts the number of alive neighbours
 			for m := -1; m <= 1; m++ {
@@ -35,23 +35,28 @@ func worker(world [][]byte, p Params, c distributorChannels, turn int, startY, e
 						dx := (j + n + p.ImageWidth) % p.ImageWidth
 
 						// byte values of 255 to 1 for proccessing
-						sum += (int(world[dy][dx]) / 255)
+						sum += (uint8(world[dy][dx]) / 255)
 					}
 				}
 			}
+			
+      var oldstate uint8 = world[worldIndex][j] / 255
 
 			// apply rules corresponding to the total surronding alive
 			// cells in context for the state of the current cell
-
-			oldstate := world[worldIndex][j]
-
-			if sum == 3 || (int(world[worldIndex][j])/255)+sum == 3 {
-				newworld[i][j] = 255
-			}
-
-			if oldstate != newworld[i][j] {
-				c.events <- CellFlipped{CompletedTurns: turn, Cell: util.Cell{X: j, Y: worldIndex}}
-			}
+      // ensures flipping of cells only if a change is present
+      if oldstate == 1 {
+        if sum == 2 || sum == 3 {
+				  newworld[i][j] = 255
+        } else {
+				  c.events <- CellFlipped{CompletedTurns: turn, Cell: util.Cell{X: j, Y: worldIndex}}
+        }
+      } else {
+        if sum == 3 {
+          newworld[i][j] = 255
+          c.events <- CellFlipped{CompletedTurns: turn, Cell: util.Cell{X: j, Y: worldIndex}}
+        }
+      }
 		}
 	}
 	// send computed data to the channel provided by the arguments
